280f80e9838b56f196ef50bf90456564c455b15b
haarish04 <haarish.anandan@gmail.com>
2024-04-28 00:30:04 +0530

    Updated main and InputFileReader files and implemented reading the file and printing the contents.

--- src/InputFileReader.java
+++ src/InputFileReader.java
@@ -1,3 +1,26 @@
+import java.nio.file.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+
 public class InputFileReader {
+
+    public void readFile(String filePath) throws IOException{
+
+        Path path = Paths.get(filePath);
+        BufferedReader br = null;
+
+        try{
+            br= Files.newBufferedReader(path);
+            String line= br.readLine();
+    
+            System.out.println(line);
+        }
+        finally{
+            if(br != null)
+                br.close();
+        }
+
+    }
     
 }
--- src/main.java
+++ src/main.java
@@ -1,3 +1,15 @@
-public class main {
-    
+public class Main {
+
+    public static void main(String[] args){
+        String filePath= args[0];
+        System.out.println(filePath);
+        try{
+            InputFileReader input = new InputFileReader();
+            input.readFile(filePath);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
