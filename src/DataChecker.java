84b93844a2b4cf79499e4c68de36424d1efa5432
Haarish Anandan <131394736+haarish04@users.noreply.github.com>
2024-05-12 00:55:55 +0530

    Update DataChecker.java

--- src/DataChecker.java
+++ src/DataChecker.java
@@ -1,4 +1,5 @@
 public class DataChecker {
+    //Compare original text and final decoded text
     public String dataCheck(String beforeEncoding, String afterDecoding){
         if(beforeEncoding.equals(afterDecoding))
             return "Data integrity has been verified.\nThe original file and the decoded file match\n";
