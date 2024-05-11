aa6641969723ca790bf1ff6b6a1ddf79a1d4a068
Haarish Anandan <131394736+haarish04@users.noreply.github.com>
2024-05-12 00:55:18 +0530

    Update DataSize.java

--- src/DataSize.java
+++ src/DataSize.java
@@ -2,6 +2,8 @@ import java.nio.charset.StandardCharsets;
 import java.util.BitSet;
 
 public class DataSize {
+    //Use object datatype to handle both string in case of original file
+    //and bitSet datatype and perform type guarding
     public int getFileSize(Object data){
         if (data instanceof String) {
             final byte[] utf8Bytes = ((String) data).getBytes(StandardCharsets.UTF_8);
